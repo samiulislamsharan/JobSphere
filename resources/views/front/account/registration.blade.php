b164a94b4dafd06209393bb403f90c7705b97218
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-09 22:16:09 +0600

    feat(views): :lipstick: add registration blade for displaying registration page

---
+++ resources/views/front/account/registration.blade.php
@@ -0,0 +1,147 @@
+@extends('front.layouts.app')
+
+@section('content')
+    <section class="section-5">
+        <div class="container my-5">
+            <div class="py-lg-2">&nbsp;</div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-5">
+                    <div class="card shadow border-0 p-5">
+                        <h1 class="h3">Register</h1>
+                        <form action="" name="registrationForm" id="registrationForm" autocomplete="on">
+                            <div class="mb-3">
+                                <label for="name" class="mb-2">Name*</label>
+                                <input type="text" name="name" id="name" class="form-control"
+                                    placeholder="Enter Name" autocomplete="on">
+                                <p></p>
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="mb-2">Email*</label>
+                                <input type="text" name="email" id="email" class="form-control"
+                                    placeholder="Enter Email" autocomplete="on">
+                                <p></p>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="mb-2">Password*</label>
+                                <input type="password" name="password" id="password" class="form-control"
+                                    placeholder="Enter Password">
+                                <p></p>
+                            </div>
+                            <div class="mb-3">
+                                <label for="confirm_password" class="mb-2">Confirm Password*</label>
+                                <input type="password" name="confirm_password" id="confirm_password" class="form-control"
+                                    placeholder="Confirm Password">
+                                <p></p>
+                            </div>
+                            <button class="btn btn-primary mt-2">Register</button>
+                        </form>
+                    </div>
+                    <div class="mt-4 text-center">
+                        <p>Have an account? <a href="{{ route('account.login') }}">Login</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('customJS')
+    <script>
+        // jQuery script to register form using AJAX
+        $("#registrationForm").submit(function(e) {
+            e.preventDefault();
+
+            $.ajax({
+                url: "{{ route('account.registerUser') }}",
+                type: "POST",
+                data: $("#registrationForm").serializeArray(),
+                dataType: "JSON",
+                success: function(response) {
+                    if (response.status === false) {
+                        var errors = response.errors;
+
+                        if (errors.name) {
+                            $("#name")
+                                .addClass('is-invalid')
+                                .siblings('p')
+                                .addClass('invalid-feedback')
+                                .html(errors.name);
+                        } else {
+                            $("#name")
+                                .removeClass('is-invalid')
+                                .siblings('p')
+                                .removeClass('invalid-feedback')
+                                .html('');
+                        }
+
+                        if (errors.email) {
+                            $("#email")
+                                .addClass('is-invalid')
+                                .siblings('p')
+                                .addClass('invalid-feedback')
+                                .html(errors.email);
+                        } else {
+                            $("#email")
+                                .removeClass('is-invalid')
+                                .siblings('p')
+                                .removeClass('invalid-feedback')
+                                .html('');
+                        }
+
+                        if (errors.password) {
+                            $("#password")
+                                .addClass('is-invalid')
+                                .siblings('p')
+                                .addClass('invalid-feedback')
+                                .html(errors.password);
+                        } else {
+                            $("#password")
+                                .removeClass('is-invalid')
+                                .siblings('p')
+                                .removeClass('invalid-feedback')
+                                .html('');
+                        }
+
+                        if (errors.confirm_password) {
+                            $("#confirm_password")
+                                .addClass('is-invalid')
+                                .siblings('p')
+                                .addClass('invalid-feedback')
+                                .html(errors.confirm_password);
+                        } else {
+                            $("#confirm_password")
+                                .removeClass('is-invalid')
+                                .siblings('p')
+                                .removeClass('invalid-feedback')
+                                .html('');
+                        }
+                    } else {
+                        $("#name")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
+
+                        $("#email")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
+
+                        $("#password")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
+
+                        $("#confirm_password")
+                            .removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('');
+                    }
+                }
+            });
+        });
+    </script>
+@endsection
