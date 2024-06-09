b995dcc88b1ac8c33d4cca8108ec4ebbf14699a7
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-09 22:10:20 +0600

    feat(routes): :sparkles: add home and account login, registration routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/account/register', [AccountController::class, 'registration'])->name('account.registration');
+Route::post('/account/registerUser', [AccountController::class, 'registerUser'])->name('account.registerUser');
+Route::get('/account/login', [AccountController::class, 'login'])->name('account.login');
