0bb32dd397606f0e7e7a4ae76568ca4c10b9acb3
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-09 22:11:54 +0600

    feat(migrations): :sparkles: create users table with extended attributes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image')->nullable();
+            $table->string('designation')->nullable();
+            $table->string('mobile')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
