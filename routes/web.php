06e63f808d3e599ed76237c51d3ce9b51e642a08
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-11 00:01:30 +0600

    feat(routes): :sparkles: add routes for account login authentication, and profile

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
 Route::get('/account/register', [AccountController::class, 'registration'])->name('account.registration');
 Route::post('/account/registerUser', [AccountController::class, 'registerUser'])->name('account.registerUser');
+
 Route::get('/account/login', [AccountController::class, 'login'])->name('account.login');
+
+Route::post('/account/auth', [AccountController::class, 'authenticate'])->name('account.auth');
+
+Route::get('/account/profile', [AccountController::class, 'profile'])->name('account.profile');
