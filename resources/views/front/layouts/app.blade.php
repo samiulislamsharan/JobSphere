eb30882518bb8fdb772b23b72a2865793e8a17d3
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-11 14:44:13 +0600

    refactor(views): separate navbar to blade file and add to master layout

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -18,29 +18,7 @@
 
 <body data-instant-intensity="mousedown">
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-white shadow py-3">
-            <div class="container">
-                <a class="navbar-brand" href="{{ route('home') }}">JobSphere</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-0 ms-sm-0 me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="index.html">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="jobs.html">Find Jobs</a>
-                        </li>
-                    </ul>
-                    <a class="btn btn-outline-primary me-2" href="{{ route('account.showLogin') }}"
-                        type="submit">Login</a>
-                    <a class="btn btn-primary" href="post-job.html" type="submit">Post a Job</a>
-                </div>
-            </div>
-        </nav>
+        @include('front.layouts.shared.navbar')
     </header>
 
     @yield('content')
