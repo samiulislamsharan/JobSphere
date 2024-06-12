367c733414871f2efa2203b4fe58c7a6e5ada2a8
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-12 17:45:57 +0600

    refactor(middleware): :recycle: update redirectTo method to use account.login.index route

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('account.showLogin');
+        return $request->expectsJson() ? null : route('account.login.index');
     }
 }
