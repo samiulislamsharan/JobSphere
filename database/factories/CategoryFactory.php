b58a19b6a61a37a2c6c4cf57d28ce639957d4054
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-14 16:07:03 +0600

    feat(factories): :sparkles: add Category factory to store job categories in DB

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->randomElement(
+                [
+                    'Engineering',
+                    'Accountant',
+                    'Information Technology',
+                    'Fashion designing'
+                ]
+            ),
+        ];
+    }
+}
