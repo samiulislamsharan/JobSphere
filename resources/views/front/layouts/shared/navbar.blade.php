8fbd9daf1ce702d8586e8076e5b5a7fef64e07f2
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-14 11:52:27 +0600

    refactor(views): :recycle: update home page link in collapsable navbar

--- resources/views/front/layouts/shared/navbar.blade.php
+++ resources/views/front/layouts/shared/navbar.blade.php
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-0 ms-sm-0 me-auto mb-2 mb-lg-0 ms-lg-4">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="index.html">Home</a>
+                    <a class="nav-link" aria-current="page" href="{{ route('home') }}">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="jobs.html">Find Jobs</a>
