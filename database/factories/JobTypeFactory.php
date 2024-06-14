7f5b2263c82aa9ecdeae381c07b3466e1b0e4ca4
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-14 16:07:51 +0600

    feat(factories): :sparkles: add JobType factory to store job types in DB

---
+++ database/factories/JobTypeFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class JobTypeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->randomElement(
+                [
+                    'Full Time',
+                    'Part Time',
+                    'Remote',
+                    'Freelance'
+                ]
+            ),
+        ];
+    }
+}
