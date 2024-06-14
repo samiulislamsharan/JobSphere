d6b1c3cfdd89cc5bd7c5e99bda0c0823678717f6
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-14 16:14:48 +0600

    feat(seeders): :sparkles: add Category and JobType factories and models to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Category::factory(4)->create();
+        \App\Models\JobType::factory(4)->create();
     }
 }
