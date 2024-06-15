46da3e92b7eef279d338fb54436e9ce09b8dee22
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-15 17:05:36 +0600

    feat(controllers): :sparkles: update saveJob method in AccountController
    The `saveJob` method handles the submission of the job creation form. It validates the job details and saves the job to the database. If the validation passes, it returns a success response with a session flash message.

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Job;
 use App\Models\JobType;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -211,6 +212,32 @@ class AccountController extends Controller
         $validator = Validator::make($request->all(), $rules);
 
         if ($validator->passes()) {
+            $job = new Job();
+
+            $job->title = $request->title;
+            $job->category_id = $request->category;
+            $job->job_type_id = $request->job_type;
+            $job->vacancy = $request->vacancy;
+            $job->salary = $request->salary;
+            $job->location = $request->location;
+            $job->description = $request->description;
+            $job->benefits = $request->benefits;
+            $job->responsibility = $request->responsibility;
+            $job->qualifications = $request->qualifications;
+            $job->keywords = $request->keywords;
+            $job->experience = $request->experience;
+            $job->company_name = $request->company_name;
+            $job->company_location = $request->company_location;
+            $job->company_website = $request->company_website;
+
+            $job->save();
+
+            session()->flash('success', 'Job created successfully!');
+
+            return response()->json([
+                'status' => true,
+                'errors' => [],
+            ]);
         } else {
             return response()->json([
                 'status' => false,
