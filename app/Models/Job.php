a4343d337c7470955c19ce60b60156e02a23acb5
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-15 17:03:05 +0600

    feat(models): :sparkles: add Job model
    The Job model is added to the repository. This model represents a job listing and includes various attributes such as title, category ID, job type ID, vacancy number, salary, location, description, benefits, responsibility, qualifications, keywords, experience, company name, company location, and company website. This model will be used to interact with the database and perform CRUD operations on job listings.

---
+++ app/Models/Job.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'category_id',
+        'job_type_id',
+        'vacancy',
+        'salary',
+        'location',
+        'description',
+        'benefits',
+        'responsibility',
+        'qualifications',
+        'keywords',
+        'experience',
+        'company_name',
+        'company_location',
+        'company_website',
+    ];
+}
