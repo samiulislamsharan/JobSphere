5ea3769a6c16af6789c398032263088e622a916f
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-16 16:47:38 +0600

    feat(controllers): :sparkles: add user_id to saveJob method in AccountController

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -217,6 +217,7 @@ class AccountController extends Controller
             $job->title = $request->title;
             $job->category_id = $request->category;
             $job->job_type_id = $request->job_type;
+            $job->user_id = Auth::user()->id;
             $job->vacancy = $request->vacancy;
             $job->salary = $request->salary;
             $job->location = $request->location;
