6760e3a02f32d058d1e6f59894ea2c017fb9a700
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-18 00:13:15 +0600

    fix(controllers): :bug: add orderBy method to fetch latest jobs from DB

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -249,7 +249,7 @@ class AccountController extends Controller
 
     public function myJobs()
     {
-        $jobs = Job::where('user_id', Auth::user()->id)->with('jobType')->paginate(10);
+        $jobs = Job::where('user_id', Auth::user()->id)->with('jobType')->orderBy('created_at', 'DESC')->paginate(10);
 
         return view(
             'front.account.job.my-jobs',
