feb880bcef049a44dc65c1cb721d6d6297198f9c
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-18 01:04:07 +0600

    feat(routes): :sparkles: add /jobs route in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\JobsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/jobs', [JobsController::class, 'index'])->name('jobs');
 
 Route::group(['prefix' => 'account', 'as' => 'account.'], function () {
     // guest routes
