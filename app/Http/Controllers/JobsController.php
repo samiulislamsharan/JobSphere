0392fe2e3eb103a315805b78da92f7e64e5ad22d
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-18 17:19:42 +0600

    feat(controllers): :sparkles: add category, job type, and job data to JobsController@index

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Job;
+use App\Models\JobType;
 use Illuminate\Http\Request;
 
 class JobsController extends Controller
 {
     public function index()
     {
-        return view('front.jobs');
+        $categories = Category::where('status', 1)->orderBy('name', 'ASC')->get();
+        $jobTypes = JobType::where('status', 1)->orderBy('name', 'ASC')->get();
+        $jobs = Job::where('status', 1)->with('jobType')->orderBy('created_at', 'DESC')->paginate(9);
+
+        return view(
+            'front.jobs',
+            compact(
+                'jobs',
+                'jobTypes',
+                'categories',
+            )
+        );
     }
 }
