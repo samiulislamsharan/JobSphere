dd459dd69d6517746c32944582634d0d706e5cc6
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-19 17:06:24 +0600

    chore(views): Update app.blade.php to include Font Awesome kit
    This commit updates the `app.blade.php` file to include the Font Awesome kit using a script tag. This allows the usage of Font Awesome icons throughout the application.

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}" />
+    <script src="https://kit.fontawesome.com/b9b86b707b.js" crossorigin="anonymous"></script>
     <!-- Fav Icon -->
     <link rel="shortcut icon" type="image/x-icon" href="#" />
 </head>
