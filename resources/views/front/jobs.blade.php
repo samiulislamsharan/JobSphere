9aeb5339c62a56fea51bfa9a734c276d0f32c19d
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-19 17:26:43 +0600

    refactor(views): :recycle: Update job detail link in jobs.blade.php to use route instead of hardcoded URL

--- resources/views/front/jobs.blade.php
+++ resources/views/front/jobs.blade.php
@@ -133,7 +133,8 @@
                                                     </div>
 
                                                     <div class="d-grid mt-3">
-                                                        <a href="job-detail.html" class="btn btn-primary btn-lg">Details</a>
+                                                        <a href="{{ route('job.detail', $job->id) }}"
+                                                            class="btn btn-primary btn-lg">Details</a>
                                                     </div>
                                                 </div>
                                             </div>
