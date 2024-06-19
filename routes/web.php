3320d5e0cbd2203f2a9451aa2e037ece1ba8af02
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-19 17:28:45 +0600

    feat(routes): :sparkles: Add route for job detail page
    The commit adds a new route `/jobs/detail/{id}` to the `web.php` file. This route is responsible for displaying the details of a specific job.

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/jobs', [JobsController::class, 'index'])->name('jobs');
+Route::get('/jobs/detail/{id}', [JobsController::class, 'detail'])->name('job.detail');
 
 Route::group(['prefix' => 'account', 'as' => 'account.'], function () {
     // guest routes
