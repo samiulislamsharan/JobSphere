0820031045fb173a658492d04a368df2428753c9
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-19 15:24:41 +0600

    feat(controllers): :sparkles: Add keyword, location, category, job type, and experience filters to JobsController@index

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -9,11 +9,54 @@ use Illuminate\Http\Request;
 
 class JobsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $categories = Category::where('status', 1)->orderBy('name', 'ASC')->get();
         $jobTypes = JobType::where('status', 1)->orderBy('name', 'ASC')->get();
-        $jobs = Job::where('status', 1)->with('jobType')->orderBy('created_at', 'DESC')->paginate(9);
+        $jobs = Job::where('status', 1);
+
+        // keyword filter
+        if (!empty($request->keywords)) {
+            $jobs = $jobs->where(function ($query) use ($request) {
+                $query->orWhere('title', 'LIKE', '%' . $request->keywords . '%');
+                $query->orWhere('keywords', 'LIKE', '%' . $request->keywords . '%');
+            });
+        }
+
+        // location filter
+        if (!empty($request->location)) {
+            $jobs = $jobs->where('location', $request->location);
+        }
+
+        // category filter
+        if (!empty($request->category)) {
+            $jobs = $jobs->where('category_id', $request->category);
+        }
+
+        // job type filter
+        $jobTypeArray = [];
+
+        if (!empty($request->jobType)) {
+            $jobTypeArray = explode(',', $request->jobType);
+
+            $jobs = $jobs->whereIn('job_type_id', $jobTypeArray);
+        }
+
+        // experience filter
+        if (!empty($request->experience)) {
+
+            $jobs = $jobs->where('experience', $request->experience);
+        }
+
+        $jobs = $jobs->with('jobType');
+
+        if (!empty($request->sort) && $request->sort == 1) {
+            $jobs = $jobs->orderBy('created_at', 'ASC');
+        } else {
+            $jobs = $jobs->orderBy('created_at', 'DESC');
+        }
+
+        $jobs = $jobs->paginate(9);
 
         return view(
             'front.jobs',
@@ -21,6 +64,7 @@ class JobsController extends Controller
                 'jobs',
                 'jobTypes',
                 'categories',
+                'jobTypeArray',
             )
         );
     }
