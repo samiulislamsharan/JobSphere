750b72ad0c8d1b8d993a87d1d7ce05916fc55e18
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-22 17:08:19 +0600

    feat(models): :sparkles: add job relationship to JobApplication model

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class JobApplication extends Model
 {
     use HasFactory;
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
 }
