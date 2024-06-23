4bdc2c060f4d869789c4e4cc12f08c6634ddf5b3
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-23 20:43:40 +0600

    feat(models): :sparkles: add user relationship to JobApplication model

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -13,4 +13,9 @@ class JobApplication extends Model
     {
         return $this->belongsTo(Job::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
