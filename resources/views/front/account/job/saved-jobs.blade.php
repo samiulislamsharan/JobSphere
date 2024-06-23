cfab1764a13936293484dbf4d7a3b525621425bd
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-23 11:49:55 +0600

    feat(views): :sparkles: add saved-jobs.blade.php to view saved jobs by user

---
+++ resources/views/front/account/job/saved-jobs.blade.php
@@ -0,0 +1,73 @@
+@extends('front.layouts.app')
+
+@section('content')
+    <section class="section-5 bg-2">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col">
+                    <nav aria-label="breadcrumb" class=" rounded-3 p-3 mb-4">
+                        <ol class="breadcrumb mb-0">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Saved Jobs</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    @include('front.account.shared.sidebar')
+                </div>
+                <div class="col-lg-9">
+                    <div class="card border-0 shadow mb-4 p-3">
+                        <div class="card-body card-form">
+                            <h3 class="fs-4 mb-1">Saved Jobs</h3>
+                            <div class="table-responsive">
+                                <table class="table ">
+                                    <thead class="bg-light">
+                                        <tr>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">Job Created</th>
+                                            <th scope="col">Applicants</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="border-0">
+                                        <tr class="active">
+                                            <td>
+                                                <div class="job-name fw-500">Web Developer</div>
+                                                <div class="info1">Fulltime . Noida</div>
+                                            </td>
+                                            <td>05 Jun, 2024</td>
+                                            <td>130 Applications</td>
+                                            <td>
+                                                <div class="job-status text-capitalize">active</div>
+                                            </td>
+                                            <td>
+                                                <div class="action-dots float-end">
+                                                    <a href="#" class="" data-bs-toggle="dropdown"
+                                                        aria-expanded="false">
+                                                        <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                                                    </a>
+                                                    <ul class="dropdown-menu dropdown-menu-end">
+                                                        <li><a class="dropdown-item" href="job-detail.html"> <i
+                                                                    class="fa fa-eye" aria-hidden="true"></i> View</a></li>
+                                                        <li><a class="dropdown-item" href="#"><i class="fa fa-trash"
+                                                                    aria-hidden="true"></i> Remove</a></li>
+                                                    </ul>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('customJS')
+@endsection
