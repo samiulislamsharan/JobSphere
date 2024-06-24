7b5449c3ca32b6fff2ad7d8e3b23679cf2ce80f5
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-24 11:54:32 +0600

    refactor(views): :recycle: update change-password-form in profile.blade.php
    This commit refactors the change-password-form in profile.blade.php to improve the structure and readability of the code. It adds proper form element names and IDs for the old password, new password, and confirm password fields. It also includes appropriate validation and error handling logic in the JavaScript code. This enhancement improves the functionality and user experience of the password change feature in the account profile page.

--- resources/views/front/account/profile.blade.php
+++ resources/views/front/account/profile.blade.php
@@ -53,25 +53,33 @@
                     </div>
 
                     <div class="card border-0 shadow mb-4">
-                        <div class="card-body p-4">
-                            <h3 class="fs-4 mb-1">Change Password</h3>
-                            <div class="mb-4">
-                                <label for="old-password" class="mb-2">Old Password*</label>
-                                <input type="password" id="old-password" placeholder="Old Password" class="form-control">
+                        <form action="" method="POST" id="change-password-form" name="change-password-form"
+                            autocomplete="on">
+                            <div class="card-body p-4">
+                                <h3 class="fs-4 mb-1">Change Password</h3>
+                                <div class="mb-4">
+                                    <label for="old_password" class="mb-2">Old Password*</label>
+                                    <input type="password" name="old_password" id="old_password" placeholder="Old Password"
+                                        class="form-control" autocomplete="on">
+                                    <p></p>
+                                </div>
+                                <div class="mb-4">
+                                    <label for="new_password" class="mb-2">New Password*</label>
+                                    <input type="password" name="new_password" id="new_password" placeholder="New Password"
+                                        class="form-control" autocomplete="on">
+                                    <p></p>
+                                </div>
+                                <div class="mb-4">
+                                    <label for="confirm_password" class="mb-2">Confirm Password*</label>
+                                    <input type="password" name="confirm_password" id="confirm_password"
+                                        placeholder="Confirm Password" class="form-control" autocomplete="on">
+                                    <p></p>
+                                </div>
                             </div>
-                            <div class="mb-4">
-                                <label for="new-password" class="mb-2">New Password*</label>
-                                <input type="password" id="new-password" placeholder="New Password" class="form-control">
+                            <div class="card-footer p-4">
+                                <button type="submit" class="btn btn-primary">Update</button>
                             </div>
-                            <div class="mb-4">
-                                <label for="confirm-password" class="mb-2">Confirm Password*</label>
-                                <input type="password" id="confirm-password" placeholder="Confirm Password"
-                                    class="form-control">
-                            </div>
-                        </div>
-                        <div class="card-footer  p-4">
-                            <button type="submit" class="btn btn-primary">Update</button>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -132,6 +140,83 @@
                                 .siblings('p')
                                 .removeClass('invalid-feedback')
                                 .html('');
+                        }
+                    }
+                }
+            });
+        });
+
+        $("#change-password-form").submit(function(e) {
+            e.preventDefault();
+
+            $.ajax({
+                type: "POST",
+                url: "{{ route('account.password.update') }}",
+                data: $("#change-password-form").serializeArray(),
+                dataType: "JSON",
+                success: function(response) {
+                    if (response.status == true) {
+
+                        $("#old_password").removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('')
+
+                        $("#new_password").removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('')
+
+                        $("#confirm_password").removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html('')
+
+                        window.location.href = "{{ route('account.profile.show') }}";
+
+                    } else {
+                        var errors = response.errors;
+
+                        if (errors.old_password) {
+                            $("#old_password")
+                                .addClass('is-invalid')
+                                .siblings('p')
+                                .addClass('invalid-feedback')
+                                .html(errors.old_password)
+                        } else {
+                            $("#old_password")
+                                .removeClass('is-invalid')
+                                .siblings('p')
+                                .removeClass('invalid-feedback')
+                                .html('')
+                        }
+
+                        if (errors.new_password) {
+                            $("#new_password")
+                                .addClass('is-invalid')
+                                .siblings('p')
+                                .addClass('invalid-feedback')
+                                .html(errors.new_password)
+                        } else {
+                            $("#new_password")
+                                .removeClass('is-invalid')
+                                .siblings('p')
+                                .removeClass('invalid-feedback')
+                                .html('')
+                        }
+
+                        if (errors.confirm_password) {
+                            $("#confirm_password")
+                                .addClass('is-invalid')
+                                .siblings('p')
+                                .addClass('invalid-feedback')
+                                .html(errors.confirm_password)
+                        } else {
+                            $("#confirm_password")
+                                .removeClass('is-invalid')
+                                .siblings('p')
+                                .removeClass('invalid-feedback')
+                                .html('')
                         }
                     }
                 }
