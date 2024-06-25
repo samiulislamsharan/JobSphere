b5063f63d65b7404b575ae37ac7ff018fbcd9d6c
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-26 00:20:20 +0600

    feat(views): :sparkles: add admin sidebar view
    This commit adds the admin sidebar view, which includes a card component with a list of links for navigation within the admin section. The sidebar is integrated into the existing admin dashboard view to improve the user experience and provide easy access to different sections of the admin panel.

---
+++ resources/views/admin/shared/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div class="card account-nav border-0 shadow mb-4 mb-lg-0">
+    <div class="card-body p-0">
+        <ul class="list-group list-group-flush ">
+            <li class="list-group-item d-flex justify-content-between p-3">
+                <a href="{{ route('account.profile.show') }}">Users</a>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                <a href="{{ route('account.job.my') }}">Jobs</a>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                <a href="{{ route('account.job.applied') }}">Jobs Applications</a>
+            </li>
+            <li class="list-group-item d-flex justify-content-center align-items-center p-3">
+                <a href="{{ route('account.logout') }}">Logout</a>
+            </li>
+        </ul>
+    </div>
+</div>
