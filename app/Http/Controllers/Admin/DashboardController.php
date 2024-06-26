0a29fa26179914c56a0517d159ade34e6afeafec
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-26 11:41:52 +0600

    refactor(controllers): :recycle: update DashboardController to include counts for users, jobs, applicants, categories, and job types
    This commit updates the DashboardController to include counts for users, jobs, applicants, categories, and job types. These counts are retrieved from the respective models and passed to the 'admin.dashboard' view. This change improves the admin dashboard by displaying important statistics at a glance.

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Job;
+use App\Models\JobApplication;
+use App\Models\JobType;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $usersCount = User::count();
+        $jobsCount = Job::where('status', 1)->count();
+        $applicantCount = JobApplication::count();
+        $categoryCount = Category::count();
+        $jobTypeCount = JobType::count();
+
+        return view(
+            'admin.dashboard',
+            compact(
+                'usersCount',
+                'jobsCount',
+                'applicantCount',
+                'categoryCount',
+                'jobTypeCount',
+            )
+        );
     }
 }
