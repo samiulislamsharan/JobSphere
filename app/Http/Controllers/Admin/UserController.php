f69ee3702262f6f50f169ba9469afa3fa64ec504
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-26 11:51:12 +0600

    feat(controllers): :sparkles: add UserController with index method
    This commit adds the UserController to the Admin namespace. The controller includes an index method that retrieves users from the User model and paginates them. The retrieved users are then passed to the 'admin.users.show-list' view. This addition improves the organization and structure of the codebase by separating the user-related logic into its own controller.

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::orderBy('created_at', 'DESC')->paginate(10);
+
+        return view('admin.users.show-list', compact('users'));
+    }
+}
