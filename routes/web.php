3ad567d439e71d24b98b12d3349c00ff5951a7bd
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-26 11:52:48 +0600

    feat(routes): :sparkles: add users.show route to the admin namespace to view all the users in the system

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobsController;
 use Illuminate\Support\Facades\Route;
@@ -21,6 +22,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth', 'admin']], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/users-list', [UserController::class, 'index'])->name('users.show');
 });
 
 Route::group(['prefix' => 'jobs'], function () {
