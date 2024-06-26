d6e61a07d51122000901be6c3f376455a3688332
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-26 11:51:31 +0600

    feat(views): :sparkles: add admin users list view
    This commit adds the 'admin.users.show-list' view, which displays a table of users with their ID, name, email, mobile, and actions. The view is integrated into the admin dashboard and improves the user experience by providing easy access to user information and actions.

---
+++ resources/views/admin/users/show-list.blade.php
@@ -0,0 +1,96 @@
+@extends('front.layouts.app')
+
+@section('content')
+    <section class="section-5 bg-2">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col">
+                    <nav aria-label="breadcrumb" class=" rounded-3 p-3 mb-4">
+                        <ol class="breadcrumb mb-0">
+                            <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                            <li class="breadcrumb-item active">Users List</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    @include('admin.shared.sidebar')
+                </div>
+                <div class="col-lg-9">
+                    @include('front.account.shared.message')
+                    <div class="card border-0 shadow mb-4 p-3">
+                        <div class="card-body card-form">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead class="bg-light">
+                                        <tr>
+                                            <th scope="col">ID</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Email</th>
+                                            <th scope="col">Mobile</th>
+                                            <th scope="col">Actions</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody class="border-0">
+                                        @forelse ($users as $user)
+                                            <tr class="active">
+                                                <td>
+                                                    <div class="fw-500">{{ $user->id }}</div>
+                                                </td>
+                                                <td>{{ $user->name }}</td>
+                                                <td>{{ $user->email }}</td>
+                                                <td>{{ $user->mobile ?? 'N/A' }}</td>
+                                                <td>
+                                                    <div
+                                                        class="action-dots d-flex justify-content-center align-items-center">
+                                                        <button href="#" class="button btn btn-secondary"
+                                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                                            <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                                                        </button>
+                                                        <ul class="dropdown-menu dropdown-menu-end">
+                                                            <li>
+                                                                <a class="dropdown-item" href="#">
+                                                                    <i class="fa fa-eye" aria-hidden="true"></i>
+                                                                    View
+                                                                </a>
+                                                            </li>
+                                                            <li>
+                                                                <a class="dropdown-item" href="#">
+                                                                    <i class="fa fa-edit" aria-hidden="true"></i>
+                                                                    Edit
+                                                                </a>
+                                                            </li>
+                                                            <li>
+                                                                <a class="dropdown-item" href="#">
+                                                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                                                    Delete
+                                                                </a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td colspan="5" class="text-center">No jobs found</td>
+                                            </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div>
+                                {{ $users->links() }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('customJS')
+    <script type="text/javascript"></script>
+@endsection
