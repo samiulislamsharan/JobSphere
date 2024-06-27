ec6f2facae02df24708f0ff94852ceff86be7d4e
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-27 18:47:43 +0600

    feat(controllers: :sparkles: add user update functionality to UserController
    This commit adds the `update` method to the `UserController` in the `Admin` namespace. The method handles the updating of user details based on the provided request data. It performs validation on the name and email fields, ensuring they meet the required criteria. If the validation passes, the user's data is updated and a success message is displayed. If the validation fails, an error response is returned with the validation errors. This addition enhances the user management experience by allowing administrators to easily update user information.

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -21,5 +22,41 @@ class UserController extends Controller
         $user = User::findOrFail($id);
 
         return view('admin.users.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:5|max:25',
+            // check the email unique rule with the user id to ignore the current user email
+            // if the email is not changed then it will ignore the current user email
+            // otherwise, it will check the email unique rule with the other users
+            'email' => 'required|email|unique:users,email,' . $id . ',id',
+        ]);
+
+        if ($validator->passes()) {
+            // find the user by id
+            $user = User::find($id);
+
+            // update the user data
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->designation = $request->designation;
+            $user->mobile = $request->mobile;
+
+            $user->save();
+
+            session()->flash('success', 'User profile updated successfully!');
+
+            return response()->json([
+                'status' => true,
+                'errors' => [],
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
     }
 }
