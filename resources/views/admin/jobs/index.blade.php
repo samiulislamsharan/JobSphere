23b574f221a576d5e4f75f801920001bd618330e
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-27 23:23:14 +0600

    feat(views): :sparkles: add index.blade.php to display all jobs in Admin view

---
+++ resources/views/admin/jobs/index.blade.php
@@ -0,0 +1,39 @@
+@extends('front.layouts.app')
+
+@section('content')
+    <section class="section-5 bg-2">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col">
+                    <nav aria-label="breadcrumb" class=" rounded-3 p-3 mb-4">
+                        <ol class="breadcrumb mb-0">
+                            <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                            <li class="breadcrumb-item active">Jobs List</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    @include('admin.shared.sidebar')
+                </div>
+                <div class="col-lg-9">
+                    @include('front.account.shared.message')
+                    <div class="card border-0 shadow mb-4 p-3">
+                        <div class="card-body card-form">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <h3 class="fs-4 mb-1">All Jobs</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('customJS')
+    <script type="text/javascript"></script>
+@endsection
