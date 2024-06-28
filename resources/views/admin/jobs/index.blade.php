22ec7085062d2e828bf3f1e11fc3570df207e348
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-29 00:26:55 +0600

    feat(views): :sparkles: add table for displaying jobs with pagination
    The code changes in `resources/views/admin/jobs/index.blade.php` add a table to display jobs with pagination. This improves the user experience by allowing them to navigate through multiple pages of job listings.

--- resources/views/admin/jobs/index.blade.php
+++ resources/views/admin/jobs/index.blade.php
@@ -26,6 +26,84 @@
                                     <h3 class="fs-4 mb-1">All Jobs</h3>
                                 </div>
                             </div>
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead class="bg-light">
+                                        <tr>
+                                            <th scope="col">ID</th>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">Created by</th>
+                                            <th scope="col">Job Created</th>
+                                            <th scope="col">Applicants</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody class="border-0">
+                                        @forelse ($jobs as $job)
+                                            <tr class="active">
+                                                <td>{{ $job->id }}</td>
+                                                <td>
+                                                    <div class="job-name fw-500">{{ $job->title }}</div>
+                                                    <div class="info1">
+                                                        {{ $job->jobType->name }} . {{ $job->location }}
+                                                    </div>
+                                                </td>
+                                                <td>{{ $job->user->name }}</td>
+                                                <td>{{ \Carbon\Carbon::parse($job->created_at)->format('d M, Y') }}</td>
+                                                <td>{{ $job->applications->count() }} Applications</td>
+                                                <td>
+                                                    @if ($job->status == 1)
+                                                        <div class="job-status text-capitalize">Active</div>
+                                                    @else
+                                                        <div class="job-status text-capitalize">Inactive</div>
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    <div
+                                                        class="action-dots d-flex justify-content-center align-items-center">
+                                                        <button href="#" class="button btn btn-secondary"
+                                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                                            <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                                                        </button>
+                                                        <ul class="dropdown-menu dropdown-menu-end">
+                                                            <li>
+                                                                <a class="dropdown-item"
+                                                                    href="{{ route('job.detail', $job->id) }}">
+                                                                    <i class="fa fa-eye" aria-hidden="true"></i>
+                                                                    View
+                                                                </a>
+                                                            </li>
+                                                            <li>
+                                                                <a class="dropdown-item"
+                                                                    href="{{ route('account.job.edit', $job->id) }}">
+                                                                    <i class="fa fa-edit" aria-hidden="true"></i>
+                                                                    Edit
+                                                                </a>
+                                                            </li>
+                                                            <li>
+                                                                <a class="dropdown-item" href="#"
+                                                                    onclick="deleteJob({{ $job->id }})">
+                                                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                                                    Delete
+                                                                </a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td colspan="5" class="text-center">No jobs found</td>
+                                            </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div>
+                                {{ $jobs->links() }}
+                            </div>
                         </div>
                     </div>
                 </div>
