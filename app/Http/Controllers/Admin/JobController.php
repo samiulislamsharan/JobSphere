e144f7fcdce79ac0ad424830da1160c0399761f7
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-29 00:16:49 +0600

    feat(controllers): :sparkles: eager load the job model with relations and include pagination

--- app/Http/Controllers/Admin/JobController.php
+++ app/Http/Controllers/Admin/JobController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
     public function index()
     {
-        return view('admin.jobs.index');
+        $jobs = Job::where('status', 1)
+            ->with(
+                [
+                    'user',
+                    'jobType',
+                    'jobCategory',
+                    'applications'
+                ]
+            )->orderBy('created_at', 'DESC')->paginate(10);
+
+        return view('admin.jobs.index', compact('jobs'));
+    }
     }
 }
