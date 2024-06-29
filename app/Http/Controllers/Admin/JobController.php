475f78e4dd316fcb489543619b54695e03244593
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-29 12:30:07 +0600

    refactor(controllers): :recycle: update JobController edit method
    This commit updates the `edit` method in the `JobController` to include additional functionality. It retrieves the job with the given ID and checks if it exists. If the job is not found, an error message is flashed and a JSON response with the error details is returned. Additionally, the method now retrieves the job types and categories from the database and passes them to the view for rendering.
    
    Refactor the `edit` method in the `JobController` to improve error handling and provide necessary data to the view.

--- app/Http/Controllers/Admin/JobController.php
+++ app/Http/Controllers/Admin/JobController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Job;
+use App\Models\JobType;
 use Illuminate\Http\Request;
 
 class JobController extends Controller
@@ -23,8 +25,31 @@ class JobController extends Controller
         return view('admin.jobs.index', compact('jobs'));
     }
 
-    public function edit()
+    public function edit($id)
     {
+        $job = Job::findOrFail($id);
+
+        if ($job == NULL) {
+            $message = 'Job not found!';
+            session()->flash('error', $message);
+
+            return response()->json([
+                'status' => false,
+                'message' => $message,
+            ]);
+        }
+
+        $jobTypes = JobType::orderBy('name', 'ASC')->get();
+        $categories = Category::orderBy('name', 'ASC')->get();
+
+        return view(
+            'admin.jobs.edit',
+            compact(
+                'job',
+                'jobTypes',
+                'categories',
+            )
+        );
     }
 
     public function destroy($id)
