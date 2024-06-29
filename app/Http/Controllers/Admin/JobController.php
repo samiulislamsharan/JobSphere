8f55e77b28a701adc9c92961fdd14c7982edd6ef
Samiul Islam <samiulislamsharan@gmail.com>
2024-06-29 15:59:23 +0600

    refactor(controllers): :recycle: update JobController edit method for improved error handling and data passing
    Refactor the `edit` method in the `JobController` to include additional functionality. This commit updates the method to retrieve the job with the given ID and check if it exists. If the job is not found, an error message is flashed and a JSON response with the error details is returned. Additionally, the method now retrieves the job types and categories from the database and passes them to the view for rendering.

--- app/Http/Controllers/Admin/JobController.php
+++ app/Http/Controllers/Admin/JobController.php
@@ -7,6 +7,8 @@ use App\Models\Category;
 use App\Models\Job;
 use App\Models\JobType;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class JobController extends Controller
 {
@@ -50,6 +52,56 @@ class JobController extends Controller
                 'categories',
             )
         );
+    }
+
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'title' => 'required|min:5|max:200',
+            'category' => 'required',
+            'job_type' => 'required',
+            'vacancy' => 'required|integer',
+            'location' => 'required',
+            'description' => 'required',
+            'company_name' => 'required|min:5|max:100',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->passes()) {
+            $job = Job::find($id);
+
+            $job->title = $request->title;
+            $job->category_id = $request->category;
+            $job->job_type_id = $request->job_type;
+            $job->user_id = Auth::user()->id;
+            $job->vacancy = $request->vacancy;
+            $job->salary = $request->salary;
+            $job->location = $request->location;
+            $job->description = $request->description;
+            $job->benefits = $request->benefits;
+            $job->responsibility = $request->responsibility;
+            $job->qualifications = $request->qualifications;
+            $job->keywords = $request->keywords;
+            $job->experience = $request->experience;
+            $job->company_name = $request->company_name;
+            $job->company_location = $request->company_location;
+            $job->company_website = $request->company_website;
+
+            $job->save();
+
+            session()->flash('success', 'Job updated successfully!');
+
+            return response()->json([
+                'status' => true,
+                'errors' => [],
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
     }
 
     public function destroy($id)
