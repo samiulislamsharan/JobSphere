82fc12ddac2a06b431820a4670a55571795ef60f
Samiul Islam <samiulislamsharan@gmail.com>
2024-07-01 01:16:52 +0600

    feat(controllers): :sparkles: add JobApplicationController destroy method
    Add the destroy method in the JobApplicationController to handle the deletion of job applications. If the job application is not found or the user is not authorized to delete it, an error message is displayed. Otherwise, the job application is deleted and a success message is shown. This ensures proper handling of job application deletion in the admin panel.

--- app/Http/Controllers/Admin/JobApplicationController.php
+++ app/Http/Controllers/Admin/JobApplicationController.php
@@ -19,4 +19,31 @@ class JobApplicationController extends Controller
 
         return view('admin.job-applications.index', compact('jobApplications'));
     }
+
+    public function destroy(Request $request)
+    {
+        $id = $request->id;
+        $jobApplication = JobApplication::find($id);
+
+        if ($jobApplication == NULL) {
+            $message = 'Either job application was deleted or you are not authorized to delete this job application!';
+
+            session()->flash('error', $message);
+
+            return response()->json([
+                'status' => false,
+                'message' => $message,
+            ]);
+        }
+
+        $jobApplication->delete();
+
+        $message = 'Job application deleted successfully!';
+        session()->flash('success', $message);
+
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+        ]);
+    }
 }
