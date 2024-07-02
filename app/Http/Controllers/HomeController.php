749d0495a37895e15a905ef171f08421efd72943
Samiul Islam <samiulislamsharan@gmail.com>
2024-07-03 01:56:32 +0600

    style(controllers): :recycle: add query() method and reformat the code for better readability

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,16 +10,34 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $categories = Category::where('status', 1)
+        $categories = Category::query()
+            ->where('status', 1)
             ->withCount(['jobs' => function ($query) {
                 $query->where('status', 1);
             }])
             ->orderBy('name', 'ASC')
             ->take(8)
             ->get();
-        $searchCategories = Category::where('status', 1)->orderBy('name', 'ASC')->get();
-        $featuredJobs = Job::where('status', 1)->orderBy('created_at', 'DESC')->with('jobType')->where('isFeatured', 1)->take(6)->get();
-        $latestJobs = Job::where('status', 1)->with('jobType')->orderBy('created_at', 'DESC')->take(6)->get();
+
+        $searchCategories = Category::query()
+            ->where('status', 1)
+            ->orderBy('name', 'ASC')
+            ->get();
+
+        $featuredJobs = Job::query()
+            ->where('status', 1)
+            ->orderBy('created_at', 'DESC')
+            ->with('jobType')
+            ->where('isFeatured', 1)
+            ->take(6)
+            ->get();
+
+        $latestJobs = Job::query()
+            ->where('status', 1)
+            ->with('jobType')
+            ->orderBy('created_at', 'DESC')
+            ->take(6)
+            ->get();
 
         return view(
             'front.home',
