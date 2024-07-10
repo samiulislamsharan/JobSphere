4d977b06d6fb946865bff30ac6ad6ad89f41238d
Samiul Islam <samiulislamsharan@gmail.com>
2024-07-10 19:23:36 +0600

    refactor(mail): :recycle: update email verification OTP template
    This commit refactors the email verification OTP template (`email-verification-otp.blade.php`) to improve its structure and readability. It also removes unnecessary JavaScript dependencies and adds a custom CSS style for links.

--- resources/views/email/email-verification-otp.blade.php
+++ resources/views/email/email-verification-otp.blade.php
@@ -7,10 +7,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css"
         integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://kit.fontawesome.com/b9b86b707b.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <style>
+        a {
+            color: #9c71e2;
+        }
+    </style>
 
     <title>{{ $mailData['title'] }}</title>
 </head>
@@ -18,7 +20,7 @@
 <body>
     <div class="container">
         <div class="card">
-            <div class="card-header h2">JobSphere</div>
+            <div class="card-header h2 text-center">JobSphere Email Verification</div>
             <div class="card-body">
                 <h4 class="card-title">Hi {{ $mailData['name'] }}</h4>
                 <p class="card-text">
@@ -31,9 +33,6 @@
                     <input value="{{ $mailData['otp'] }}" type="text" id="otp"
                         class="form-control text-center fs-1" placeholder="Email Verification OTP"
                         aria-label="Email Verification OTP" aria-describedby="otp-copy" readonly="true">
-                    <button class="btn btn-primary" type="button" id="otp-copy">
-                        <i class="fa-regular fa-copy"></i> Copy
-                    </button>
                 </div>
 
                 <p class="card-text">
@@ -46,30 +45,12 @@
                 <p class="card-text">Thanks</p>
                 <div class="card-text">The <span class="fw-bold">JobSphere</span> Team</div>
             </div>
-            {{-- <div class="card-footer text-muted">
-                <p class="card-text">If you're having trouble with the button above, copy and paste the URL below
-                    into your web browser.</p>
-                <p class="card-text"><a href="javascript:void(0);" class="link-offset-3">{{ $mailData['actionUrl'] }}</a></p>
-            </div> --}}
+            <div class="card-footer text-muted">
+                <!-- copyright text -->
+                <div class="text-center">Copyright &copy; 2024 <span class="fw-bold">JobSphere</span></div>
+            </div>
         </div>
-
     </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js"
-        integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-        $(document).ready(function() {
-            $('#otp-copy').click(function() {
-                var copyText = document.getElementById("otp");
-                copyText.select();
-                copyText.setSelectionRange(0, 99999);
-                document.execCommand("copy");
-
-                $('#otp-copy').html('<i class="fa-solid fa-check"></i></i> Copied');
-            });
-        });
-    </script>
 </body>
 
 </html>
