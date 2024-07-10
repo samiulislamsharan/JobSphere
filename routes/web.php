7d1c2191184764733127b8cc37298cf84e81877a
Samiul Islam <samiulislamsharan@gmail.com>
2024-07-10 19:52:24 +0600

    chore(routes): :bulb: added comments for admin route groups

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+// Admin routes
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth', 'admin']], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
