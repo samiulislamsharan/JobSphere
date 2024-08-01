de81f2076e30d10eb78e30f40e669d73f02a46c1
Samiul Islam <samiulislamsharan@gmail.com>
2024-08-02 01:49:56 +0600

    feat(controllers): :sparkles: add category update logic in CategoryController
    This commit adds the `update` method in the `CategoryController` to improve the category update logic. It adds validation using the `Validator` class to ensure that the `update_name` field is required. If the validation passes, it updates the category name in the database and returns a success message. If the validation fails, it returns the validation errors. This change enhances the reliability and user experience of the category update functionality.

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
@@ -15,6 +16,38 @@ class CategoryController extends Controller
             ->paginate(10);
 
         return view('admin.categories.index', compact('categories'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        // $id = $request->update_id;
+
+        $validator = Validator::make($request->all(), [
+            'update_name' => 'required',
+        ], [
+            'update_name.required' => 'The name field is required.',
+        ]);
+
+        if ($validator->passes()) {
+            Category::query()
+                ->where('id', $id)
+                ->update([
+                    'name' => $request->update_name,
+                ]);
+
+            $message = 'Category updated successfully!';
+            session()->flash('success', $message);
+
+            return response()->json([
+                'status' => true,
+                'message' => $message,
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
     }
 
     public function destroy(Request $request)
